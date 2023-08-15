f3bcb67db4b8222657120b082aaa42acd3aabdc0
JuanPablo0947 <142357071+JuanPablo0947@users.noreply.github.com>
2023-08-15 16:40:05 -0600

    Ejemplo de la primera clase sobre uso de Python

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Tecnológico de Monterrey\n")
+print("Bienvenido al curso de Pensamiento Computacional para Ingeniería\n")
+print("\tAtemtamemte: Los Profesores")
+print(
+    "Tecnológico de Monterrey\n\nBienvenido al curso de Pensamiento Computacional para Ingeniería\n\n\tAtemtamemte: Los Profesores"
+)
