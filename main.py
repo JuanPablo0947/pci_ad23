1f908ca14dfffa14bfe76725355cb844a16799ad
JuanPablo0947 <142357071+JuanPablo0947@users.noreply.github.com>
2023-08-25 23:58:33 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,19 @@ print("Tu promedio de disfrute es " + str(puntaje_promedio))
 """ 
 Calcular tu estado de ánimo general con la fórmula NEV 
 
-El NEV (Net Emotional Value) mide las emociones positivas y negativas para entender que es lo que impulsa la acción de preferencia en una relación de corto y largo plazo, ya que algunas emociones generan respuestas a corto y otras a largo. 
+El NEV (Net Emotional Value) mide las emociones 
+positivas y negativas para entender que es lo 
+que impulsa la acción de preferencia en una 
+relación de corto y largo plazo, ya que algunas emociones generan respuestas a corto y otras a largo. 
 
 La fórmula del NEV es: 
 nev = total de emociones positivas - total de emociones negativas 
 
-Digamos que hicimos los cálculos de todas las emociones ingresadas por el usuario—por referencia ver principal.py—y obtenemos un total de 18 emociones positivas contra 7 emociones negativas: 
+Digamos que hicimos los cálculos de 
+todas las emociones ingresadas por 
+el usuario—por referencia ver principal.py—y 
+obtenemos un total de 18 emociones positivas 
+contra 7 emociones negativas: 
 """ 
 total_emociones_pos = 18 
 total_emociones_neg = 7 
@@ -44,22 +51,35 @@ nev = total_emociones_pos - total_emociones_neg
 print("Tu valor emocional neto del dìa es: " + str(nev)) 
 
 """ 
-Informarte si las proporciones nutricionales que tuviste en el día es lo ideal:
-Digamos que calculamos por una serie de procesos complejos utilizando la teoría del curso, las kilocalorías que el usuario idealmente debería consumir en un día, eso es, digamos, 2200 kilocalorías 
+Informarte si las proporciones nutricionales 
+que tuviste en el día es lo ideal:
+Digamos que calculamos por una serie de 
+procesos complejos utilizando la teoría del curso,
+las kilocalorías que el usuario idealmente debería
+consumir en un día, eso es, digamos, 2200 
+kilocalorías.
 """ 
 kilocalorias_ideales_al_dia = 2200 
 
 # Proporciones nutricionales recomendadas 
-porcentaje_carbohidratos = 0.55 
-porcentaje_grasas = 0.30 
-porcentaje_proteinas = 0.15 
+PORCENTAJE_CARBOHIDRATOS = 0.55 
+PORCENTAJE_GRASAS = 0.30 
+PORCENTAJE_PROTEINAS = 0.15 
 
-proteinas_g = (calorias_ideales_al_dia * porcentaje_proteinas) / 4 #1 gramo de proteína = 4 kilocalorías 
-carbohidratos_g = (calorias_ideales_al_dia * porcentaje_carbohidratos) / 4 #1 gramo de carbohidrato = 4 kilocalorías 
-grasas_g = (calorias_ideales_al_dia *porcentaje_grasas) / 9 #1 gramo de grasa = 9 kilocalorías 
+#1 gramo de proteína = 4 kilocalorías 
+proteinas_g = (calorias_ideales_al_dia * porcentaje_proteinas) / 4 
+
+#1 gramo de carbohidrato = 4 kilocalorías 
+carbohidratos_g = (calorias_ideales_al_dia * porcentaje_carbohidratos) / 4 
+
+#1 gramo de grasa = 9 kilocalorías 
+grasas_g = (calorias_ideales_al_dia *porcentaje_grasas) / 9 
 
 print(f"Gramos de proteínas recomendadas: {proteinas_g} g") 
 print(f"Gramos de carbohidratos recomendados: {carbohidratos_g} g") 
 print(f"Gramos de grasas recomendadas: {grasas_g} g") 
 
-#Ambición final: Al final me gustaría correlacionar con el coeficiente de correlación los cuatro cálculos de cada ejemplo de arriba mostrado y crear una gráfica en el archivo de texto o generar una imagen que muestre lo dicho. 
+#Ambición final: Al final me gustaría correlacionar con el coeficiente 
+de correlación los cuatro cálculos de cada ejemplo de arriba mostrado 
+y crear una gráfica en el archivo de texto o generar una imagen 
+que muestre lo dicho. 
